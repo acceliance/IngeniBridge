0ee4b738bcefd33375d485e87f6ae8c9b397edb7
José Torres <github@superjose.com>
2017-07-25 11:08:10 +0200

    Add project files.

---
+++ MyCompanyDataModel/RootAsset.cs
@@ -0,0 +1,16 @@
+﻿using IngeniBridge.Core;
+using IngeniBridge.Core.MetaHelper.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCompanyDataModel
+{
+    [RootAsset]
+    public class MyCompanyRootAsset : Asset
+    {
+        public ProductionSite [] ProductionSites { get; set; }
+    }
+}
